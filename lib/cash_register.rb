58fa3186edd37a935cd287d181ae61d6c0026585
Elliot Blanchard <public@elliotblanchard.com>
2020-04-18 19:11:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,17 @@
 class CashRegister
   attr_accessor :discount, :total
   @total
+  @items
 
   def initialize(discount = 0)
     @total = 0
+    @items = []
     @discount = discount
+  end
+
+  def add_item(title,price,quantity=1)
+    @items << title
+    @total += price*quantity
   end
 
 end
