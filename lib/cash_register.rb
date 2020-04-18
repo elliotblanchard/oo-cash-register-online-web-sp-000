849f23facf380377ff1223838efc25670e83f74a
Elliot Blanchard <public@elliotblanchard.com>
2020-04-18 19:02:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
+class CashRegister
+  attr_accessor :employee_discount
+  @total
 
+  def initialize(employee_discount = 0)
+    @total = 0
+    @employee_discount = employee_discount
+  end
+  
+end
